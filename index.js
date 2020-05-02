7f99a913ea70580979e4de15f8addd27118a138a
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-02 15:23:48 +0200

    test and start script

---
+++ index.js
@@ -0,0 +1,12 @@
+const yargs = require("yargs")
+const args = yargs.argv
+const detectPort = require("detect-port")
+const fs = require("fs")
+
+
+
+console.log("hey")
+console.log(fs.existsSync("./test_out"))
+
+
+
