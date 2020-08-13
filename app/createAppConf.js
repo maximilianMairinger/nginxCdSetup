20e2ba4ee4ae661e69898e6840e0311480bc3723
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-13 18:01:10 +0200

    Revert "Truncate clone cmd"
    This reverts commit fef5d670ae4c18ff39df96b9a244d2a090d24503.

--- app/createAppConf.js
+++ app/createAppConf.js
@@ -18,7 +18,10 @@ module.exports = async (masterConfig, devConfig) => {
   console.log("started app")
 
   configs.ea((conf) => {
-    shell.exec(`cd ${path.join(conf.appDest, conf.branch)} && git clone git@github.com:${conf.githubUsername}/${conf.name} && cd ${conf.name} && git checkout ${conf.branch}`)
+    shell.cd(path.join(conf.appDest, conf.branch))
+    shell.exec(`git clone git@github.com:${conf.githubUsername}/${conf.name}`)
+    shell.exec(`cd ${conf.name}`)
+    shell.exec(`git checkout ${conf.branch}`)
   })
 
   console.log("clone done")
