d2671cec56df7384d7439274d3867ade073475a6
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-09 22:19:00 +0200

    ecosystem config file

---
+++ res/ecosystem.config.js
@@ -0,0 +1,64 @@
+const fs = require("fs")
+
+const configFilePath = ".deploy"
+
+
+const configPresets = {
+  repl: {
+    script: "replServer/dist/server.js",
+    name: "$[ branch / hash ].$[ name ]",
+    exec_mode : "cluster",
+    instances: 2,
+    wait_ready: true,
+    args: "--port $[ port ]"
+  },
+  prod: {
+    script: "server/dist/server.js",
+    name: "$[ branch / hash ].$[ name ]",
+    exec_mode : "cluster",
+    instances: 2,
+    wait_ready: true,
+    args: "--port $[ port ]"
+  }
+}
+
+
+
+
+
+function constructMergeConfig(defaultConfig) {
+  return function mergeConfig(config) {
+    for (const key in defaultConfig) {
+      if (config[key] === undefined) config[key] = defaultConfig[key]
+    }
+    return config
+  }
+}
+
+const mergeConfig = constructMergeConfig(configPresets.repl)
+
+let config
+if (fs.existsSync(configFilePath)) {
+  let rawConfig = fs.readFileSync(configFilePath).toString()
+  let isValidJSON = true
+  let jsonConfig
+  try {
+    jsonConfig = JSON.parse(rawConfig)
+    if (typeof jsonConfig !== "object") throw new Error()
+  }
+  catch(e) {
+    isValidJSON = false
+  }
+
+
+  if (isValidJSON) config = mergeConfig(jsonConfig)
+  else if (configPresets[rawConfig] !== undefined) config = configPresets[rawConfig]
+  else config = configPresets.repl
+}
+else config = configPresets.repl
+
+
+
+module.exports = {
+  apps: [config]
+}
