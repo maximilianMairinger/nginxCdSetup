12c5a7f3cc42df77706b788e2b11268e1b5c3f31
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-24 13:42:36 +0100

    log redone

--- start.js
+++ start.js
@@ -1,2 +1,38 @@
-const { go } = require("./dist/nginxCdSetup")
-go().then(() => console.log("Done."))
+const fs = require("fs")
+
+
+
+function formatLog(kind, logs) {
+  return "[" + kind + "]: " + logs.join(", ").split("\n").join("\n\t\t\t") + "\t\t\t\t\t\t\t[" + new Date().toLocaleString("de-AT", { timeZone: "Europe/Vienna" }) + "]"
+}
+function writeToLogFile(log) {
+  fs.appendFileSync("log", log + "\n")
+}
+
+function injectFsToLogLevel(level) {
+  const logLocal = console[level].bind(console)
+  console[level] = (...logs) => {
+    let formattedLog = formatLog(level, logs)
+    logLocal(formattedLog)
+    writeToLogFile(formattedLog)
+  }
+}
+
+injectFsToLogLevel("log")
+injectFsToLogLevel("warn")
+injectFsToLogLevel("error")
+
+
+try {
+  const { go } = require("./dist/nginxCdSetup")
+  console.log("")
+  console.log("")
+  console.log("Go")
+  go().then(() => console.log("Done."))
+}
+catch(e) {
+  console.error("Thrown", e)
+}
+
+
+
